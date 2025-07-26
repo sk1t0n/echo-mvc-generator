96e370579082592eb0a45bcf308bab080d306156
Anton Grabovsky <agrabovsky2020@gmail.com>
2025-07-26 15:45:28 +0300

    add tests

--- lib/lib_test.go
+++ lib/lib_test.go
@@ -30,3 +30,42 @@ func TestGetEntityName(t *testing.T) {
 		})
 	}
 }
+
+func TestIsLower(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want bool
+	}{
+		{"lower case", "test", true},
+		{"pascal case", "Test", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lib.IsLower(tt.s)
+			if got != tt.want {
+				t.Errorf("IsLower(%s) = %v, want %v", tt.s, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRemoveLastSlash(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want string
+	}{
+		{"ends with a slash", "test/", "test"},
+		{"ends with a backslash", "test\\", "test"},
+		{"does not end with a slash", "test", "test"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lib.RemoveLastSlash(tt.s)
+			if got != tt.want {
+				t.Errorf("RemoveLastSlash(%s) = %v, want %v", tt.s, got, tt.want)
+			}
+		})
+	}
+}
